bf4ed652874006571126513beafaf34bce7fbfc6
Vangie Shue <vangieshue@gmail.com>
2015-04-25 21:54:41 -0400

    Initialized basics

---
+++ src/edu/nyu/appliedcryptography/vds229/CryptosystemApp.java
@@ -0,0 +1,18 @@
+package edu.nyu.appliedcryptography.vds229;
+
+/***
+ * This is app uses a [modified] RSA public/private cryptosystem to
+ * create digital certificates and perform authentication according to the
+ * specifications given by the NYU 3205 Applied Cryptography final project. 
+ * @author Vangie
+ */
+public class CryptosystemApp {
+
+  public static void main(String[] args) {
+    new CryptosystemApp().run(); //get out of static context ASAP
+  }
+  private void run() {
+    
+  }
+
+}
