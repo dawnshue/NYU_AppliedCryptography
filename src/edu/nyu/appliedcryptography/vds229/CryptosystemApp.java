eabf9b5b80e5134e6fc2167514cb3ad13f24305c
v-shue <vangieshue@gmail.com>
2015-04-27 03:39:57 -0400

    Up to last part of section 6

--- src/edu/nyu/appliedcryptography/vds229/CryptosystemApp.java
+++ src/edu/nyu/appliedcryptography/vds229/CryptosystemApp.java
@@ -13,8 +13,18 @@ public class CryptosystemApp {
    * Immediately escapes the static context and runs the program.
    */
   public static void main(String[] args) {
-    //new CryptosystemApp().test();
-    new CryptosystemApp().run(); //get out of static context ASAP
+    CryptosystemApp alice = new CryptosystemApp();
+    alice.dontprint = false;
+    alice.debug = false;
+    alice.run();
+    
+    CryptosystemApp trent = new CryptosystemApp();
+    trent.dontprint = true;
+    trent.debug = false;
+    trent.verboseoverride = false;
+    trent.run();
+    
+    alice.run2(alice, trent);
   }
   
   /*
@@ -29,56 +39,72 @@ public class CryptosystemApp {
   //The past quotients generated while checking the GCD(e,n), used to determine d
   LinkedList<Integer> quotient = new LinkedList<Integer>();
   //The important cryptosystem values
-  //e: public key
+  //e: public key, d: private key
   public int p, q, n, e, d;
+  //For section 5, hr is hash(r), s is signature
+  public String r;
+  public int hr, s;
+  //For section 6
+  public int k, u;
+  public int hu, v, ev;
   
   /**
    * This is the main program, running through all the steps in the 
    * project specification.
    */
   private void test() {
-    this.isPrime(11,true);
+    System.out.println(this.getR("Alice", 0, 0));
   }
-  private void run() {
+  public void run() {
     /*
      * PART 4
      */
-    System.out.println("104 =======================================");
+    this.print(true,"104 =======================================");
     this.generateInitialpq();
     
-    System.out.println("119 =======================================");
+    this.print(true,"119 =======================================");
     this.getNonPrimeTrace();
     //!!!!!!!!!!!!!!!!!!!
     //ALGORITHM FOR ISPRIME NEEDS TO BE MODIFIED
     //!!!!!!!!!!!!!!!!!!!
-    System.out.println("123 =======================================");
+    this.print(true,"123 =======================================");
     this.getPrimePQ();
     
     n = p*q;
-    this.debug("### final n = "+n);
+    this.debug("-> final n = "+n);
     
-    System.out.println("142 =======================================");
+    this.print(true,"142 =======================================");
     this.getPublicKey();
     
-    System.out.println("152 =======================================");
+    this.print(true,"152 =======================================");
     this.getPrivateKey();
     
-    System.out.println("156 =======================================");
+    this.print(true,"156 =======================================");
     this.showAllValues();
-    
+  }
+  public void run2(CryptosystemApp alice, CryptosystemApp trent) {
     /*
-     * PART 5
+     * PART 5 !!!!!!!!!!!!!!!!!!!! INCOMPLETE !!!!!!!!!!!!!!!!!!!!!!
      */
     System.out.println("185 =======================================");
-    this.getDigitalCertificate();
+    this.getDigitalCertificate(alice, trent);
+    this.print(true, "h(r) = "+this.getByteRepresentation(hr)+"");
+    this.print(true, "s = "+this.get32BitRepresentation(s)+"");
     System.out.println("187 =======================================");
-    this.getDigitalCertificateAsIntegers();
+    this.print(true, "h(r) = "+hr);
+    this.print(true, "s = "+s);
     
     /*
      * PART 6
      */
     System.out.println("206 =======================================");
-    
+    this.determineKU();
+    System.out.println("208 =======================================");
+    this.print(true,"u = "+this.get32BitRepresentation(u));
+    System.out.println("215 =======================================");
+    this.getDecryptedHash();
+    System.out.println("219 =======================================");
+    this.getEncryptedHash();
   }
   
   private void print(boolean verbose, String message) {
@@ -106,24 +132,24 @@ public class CryptosystemApp {
     } while(p == q);
     pastvalues.add(p);
     pastvalues.add(q);
-    debug("### Initial p = "+p);
-    debug("### Initial q = "+q);
+    debug("-> Initial p = "+p);
+    debug("-> Initial q = "+q);
   }
   private int generateRandomNumber(boolean verbose) {
     int p = 1;
     print(verbose,"Generating a random 7-bit integer:");
-    print(verbose,"n = 1 (because last bit is 1, 1*2^0=1)");
+    print(verbose,"x = 1 (because last bit is 1, 1*2^0=1)");
     for(int i=1; i<6; i++) {
       int temp = Math.abs(randomizer.nextInt());
       print(verbose,"Random number "+i+" generated: "+temp);
       print(verbose,"Least significant bit of "+temp+" is "+temp%2);
-      print(verbose,"n = "+p+" + "+(int)Math.pow(2, i)*(temp%2));
+      print(verbose,"x = "+p+" + "+(int)Math.pow(2, i)*(temp%2));
       p = p + (int)Math.pow(2, i)*(temp%2);
-      print(verbose,"n = "+p);
+      print(verbose,"x = "+p);
     }
     print(verbose,"Most significant bit of random number is always 1.");
-    print(verbose,"n = "+p+" + "+(int)Math.pow(2, 6));
-    print(verbose,"n = "+(p+(int)Math.pow(2, 6)));
+    print(verbose,"x = "+p+" + "+(int)Math.pow(2, 6));
+    print(verbose,"x = "+(p+(int)Math.pow(2, 6)));
     p = p + (int)Math.pow(2, 6);
     return p;
   }
@@ -152,18 +178,18 @@ public class CryptosystemApp {
         p = this.generateRandomNumber(false);
       } while (pastvalues.contains(p));
       pastvalues.add(p);
-      this.debug("### p = generateRandomNumber("+p+")");
+      this.debug("-> p = generateRandomNumber("+p+")");
     }
     while(!this.isPrime(q, false)) {
       do {
         q = this.generateRandomNumber(false);
       } while (pastvalues.contains(q));
       pastvalues.add(q);
-      this.debug("### q = generateRandomNumber("+q+")");
+      this.debug("-> q = generateRandomNumber("+q+")");
     }
     this.isPrime(p,true);
-    this.print(true,"### final p = "+p);
-    this.print(true,"### final q = "+q);
+    this.print(true,"-> final p = "+p);
+    this.print(true,"-> final q = "+q);
   }
   private boolean isPrime(int n, boolean verbose) {
     //Tests if the given int is a prime number by modified Miller-Rabin
@@ -204,18 +230,18 @@ public class CryptosystemApp {
       if(e==n) {
         //No relatively prime value e found
         //Regenerate prime values p, q
-        this.print(verbose,"### No relatively prime e found. Regenerating primes p & q.");
+        this.print(verbose,"-> No relatively prime e found. Regenerating primes p & q.");
         p = this.generateRandomNumber(false);
         q = this.generateRandomNumber(false);
         this.getPrimePQ();
-        this.print(verbose,"### New p = "+p);
-        this.print(verbose,"### New q = "+q);
+        this.print(verbose,"-> New p = "+p);
+        this.print(verbose,"-> New q = "+q);
         n = p*q;
-        this.print(verbose,"### New n = "+n);
+        this.print(verbose,"-> New n = "+n);
         e = 3;
       }
     }
-    print(true,"### final e = "+e);
+    print(true,"-> final e = "+e);
   }
   private boolean isRelativelyPrime(int e, int n, boolean verbose) {
     print(true,"Trying e = "+e);
@@ -255,38 +281,141 @@ public class CryptosystemApp {
       }
     }
     d = p1;
-    print(true,"### final d = "+d);
+    print(true,"-> final d = "+d);
   }
 
   private void showAllValues() {
-    System.out.println("p = "+p+" ("+getBitRepresentation(p)+")");
-    System.out.println("q = "+q+" ("+getBitRepresentation(q)+")");
-    System.out.println("n = "+n+" ("+getBitRepresentation(n)+")");
-    System.out.println("e = "+e+" ("+getBitRepresentation(e)+")");
-    System.out.println("d = "+d+" ("+getBitRepresentation(d)+")");
+    this.print(true,"p = "+p+" ("+get32BitRepresentation(p)+")");
+    this.print(true,"q = "+q+" ("+get32BitRepresentation(q)+")");
+    this.print(true,"n = "+n+" ("+get32BitRepresentation(n)+")");
+    this.print(true,"e = "+e+" ("+get32BitRepresentation(e)+")");
+    this.print(true,"d = "+d+" ("+get32BitRepresentation(d)+")");
   }
-  private String getBitRepresentation(int x) {
-    StringBuilder s = new StringBuilder(Integer.toBinaryString(x));
-    for(int b=s.length(); b<32; b++) {
-      s.insert(0, "0");
+  private String get32BitRepresentation(int x) {
+    this.debug("get32BitRepresentation("+x+")");
+    StringBuilder str = new StringBuilder();
+    String bin = Integer.toBinaryString(x);
+    for(int b=bin.length(); b<32; b++) {
+      str.append("0");
     }
-    return s.toString();
+    str.append(bin);
+    return str.toString();
+  }
+  private String getByteRepresentation(int x) {
+    this.debug("getByteRepresentation("+x+")");
+    StringBuilder str = new StringBuilder();
+    String bin = Integer.toBinaryString(x);
+    for(int b=bin.length(); b<8; b++) {
+      str.append("0");
+    }
+    this.debug("Padded: "+str.toString());
+    str.append(bin);
+    this.debug("binary: "+bin);
+    return str.toString();
   }
 
-  private void getDigitalCertificate() {
-    //Determine r
-    
-    //Determine h(r)
-    
-    //Determine s
+  private void getDigitalCertificate(CryptosystemApp alice, CryptosystemApp trent) {
+    boolean verbose = true;
+    boolean trace = false;
+    //r
+    r = this.getR("Alice",alice.n,alice.e);
+    this.print(verbose, "r = "+r);
+    //hr
+    hr = this.getOnewayHash(r, trace);
+    //s
+    s = this.getDecryption(hr, trent.d, trent.n, trace);
   }
-  private void getDigitalCertificateAsIntegers() {
-    //Determine h(r)
+  private String getR(String name, int n, int e) {
+    //First part: name
+    StringBuilder rtemp = new StringBuilder("");
+    char[] namearr = name.toCharArray();
+    for(int i=0; i<6; i++) {
+      if(i<namearr.length) {
+        String letter = Integer.toBinaryString((int)namearr[i]);
+        //pad 0s if bit representation of char is not 8 bits
+        for(int ex=letter.length(); ex<8; ex++) {
+          rtemp.append("0");
+        }
+        rtemp.append(letter);
+      } else {
+        //pad 0s for the blanks (if name shorter than 6 chars) at the front
+        rtemp.insert(0,"00000000");
+      }
+    }
+    //Second part: n
+    rtemp.append(this.get32BitRepresentation(n));
+    //Third part: e
+    rtemp.append(this.get32BitRepresentation(e));
+    return rtemp.toString();
+  }
+  private int getOnewayHash(String x, boolean verbose) {
+    this.debug(""+x.length());
+    char[] xarr = x.toCharArray();
+    char[] curr = x.substring(0,8).toCharArray();
+    int curriter = 0;
+    for(int i=8; i<xarr.length; i++) {
+      //this.debug(i%8+": "+xarr[i]+" "+curr[i%8]+" "+new String(curr));
+      if(xarr[i] == curr[i%8]) {
+        curr[i%8] = '0';
+      } else {
+        curr[i%8] = '1';
+      }
+    }
+    this.debug(new String(curr));
+    int ans = Integer.parseInt(new String(curr),2);
+    this.debug(""+ans);
+    return ans;
+  }
+  private int getDecryption(int c, int d, int n, boolean verbose) {
+    this.debug("c^d mod(n) = "+c+"^"+d+" mod("+n+")");
+    //decrypt message m from cipher c: m = c^(d) mod(n)
+    //Use fast exponentiation algorithm given on p110 in notes, figure 64
+    String dbin = Integer.toBinaryString(d);
+    this.debug("d to binary: "+d+" -> "+dbin);
+    this.print(verbose, "y = "+c);
+    int y = c;
+    for(int i=1; i<dbin.length(); i++) {
+      this.print(verbose, "y = "+y+"^2 mod("+n+")");
+      y = (int)Math.pow(y, 2)%n;
+      if(dbin.charAt(i) == '1') {
+        this.print(verbose, "y = "+y+"*"+c+" mod("+n+")");
+        y = (c*y)%n;
+      }
+    }
+    this.print(verbose, "y = "+y);
+    return y;
+  }
+  private void determineKU() {
+    //get k
+    char[] bitn = this.get32BitRepresentation(n).toCharArray();
+    for(int c=bitn.length-1; c>=0; c--) {
+      if(bitn[c] == '1') {
+        k = c+1;
+      }
+    }
+    print(true,"k = "+k);
+    //get u
+    StringBuilder str = new StringBuilder("");
+    for(int c=0; c<k; c++) {
+      str.append("0");
+    }
+    str.append("1");
+    for(int c=0; c<(32-k-2); c++) {
+      str.append(""+Math.abs(randomizer.nextInt())%2);
+    }
+    str.append("1");
+    u = Integer.parseInt(str.toString(),2);
+    print(true,"u = "+u);
+  }
+  private void getDecryptedHash() {
+    System.out.println("u = "+u+" ("+this.get32BitRepresentation(u)+")");
+    hu = this.getOnewayHash(this.get32BitRepresentation(u),false);
+    System.out.println("h(u) = "+hu+" ("+this.getByteRepresentation(hu)+")");
     
-    //Determine s
     
   }
-
-
+  private void getEncryptedHash() {
+    
+  }
 
 }
